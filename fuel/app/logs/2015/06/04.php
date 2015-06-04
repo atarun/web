2759fec44f6f5b9b4f16ac50a5856e6832d33498
aws <atarun.0727@gmail.com>
2015-06-04 21:14:07 +0900

    fuel vender, core add

---
+++ fuel/app/logs/2015/06/04.php
@@ -0,0 +1,21 @@
+<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
+
+INFO - 2015-06-04 20:57:00 --> Fuel\Core\Request::__construct - Creating a new main Request with URI = "home"
+INFO - 2015-06-04 20:57:00 --> Fuel\Core\Request::execute - Called
+INFO - 2015-06-04 20:57:00 --> Fuel\Core\Request::execute - Setting main Request
+ERROR - 2015-06-04 20:57:00 --> Error - The requested view could not be found: template.php in /var/www/web/fuel/core/classes/view.php on line 398
+INFO - 2015-06-04 21:00:14 --> Fuel\Core\Request::__construct - Creating a new main Request with URI = "home"
+INFO - 2015-06-04 21:00:14 --> Fuel\Core\Request::execute - Called
+INFO - 2015-06-04 21:00:14 --> Fuel\Core\Request::execute - Setting main Request
+ERROR - 2015-06-04 21:00:14 --> Error - The requested view could not be found: template.php in /var/www/web/fuel/core/classes/view.php on line 398
+INFO - 2015-06-04 21:02:20 --> Fuel\Core\Request::__construct - Creating a new main Request with URI = "home"
+INFO - 2015-06-04 21:02:20 --> Fuel\Core\Request::execute - Called
+INFO - 2015-06-04 21:02:20 --> Fuel\Core\Request::execute - Setting main Request
+ERROR - 2015-06-04 21:02:20 --> Error - The requested view could not be found: template.php in /var/www/web/fuel/core/classes/view.php on line 398
+INFO - 2015-06-04 21:02:22 --> Fuel\Core\Request::__construct - Creating a new main Request with URI = "home"
+INFO - 2015-06-04 21:02:22 --> Fuel\Core\Request::execute - Called
+INFO - 2015-06-04 21:02:22 --> Fuel\Core\Request::execute - Setting main Request
+ERROR - 2015-06-04 21:02:22 --> Error - The requested view could not be found: template.php in /var/www/web/fuel/core/classes/view.php on line 398
+INFO - 2015-06-04 21:04:28 --> Fuel\Core\Request::__construct - Creating a new main Request with URI = "home"
+INFO - 2015-06-04 21:04:28 --> Fuel\Core\Request::execute - Called
+INFO - 2015-06-04 21:04:28 --> Fuel\Core\Request::execute - Setting main Request
